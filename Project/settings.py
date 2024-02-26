7ce63b4593ffa0afb1257197d5e11525f92902a8
Avishek Paul <avishek.paul@g.bracu.ac.bd>
2024-02-26 16:26:42 +0600

    'password reset using mail done might need some changes'

--- Project/settings.py
+++ Project/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'headspace799@gmail.com'
